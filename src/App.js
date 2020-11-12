1d6d48628afcb2561c671d9e2407ace86e893f85
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-11-12 11:31:00 -0700

    fake data rendering to page - api instance set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
-function App() {
-  return (
-    <h1>hello world</h1>
-  );
+import API from "./utils/API";
+import User from "./User";
+class App extends React.Component {
+  render() {
+    return <User name="Jessica Doe" avatar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Tiger_Woods_in_May_2019.jpg/220px-Tiger_Woods_in_May_2019.jpg" email="hello@jessica.com" />;
+  }
 }
 
 export default App;
