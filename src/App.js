8b5ca938c2ff7eb0a03d871e15cf1832d4a7551f
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-11-12 11:44:11 -0700

    get request now working

--- src/App.js
+++ src/App.js
@@ -2,8 +2,51 @@ import React from 'react'
 import API from "./utils/API";
 import User from "./User";
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      name: null,
+      avatar: null,
+      email: null
+    };
+  }
+
   render() {
-    return <User name="Jessica Doe" avatar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Tiger_Woods_in_May_2019.jpg/220px-Tiger_Woods_in_May_2019.jpg" email="hello@jessica.com" />;
+    const { isLoading, name, avatar, email } = this.state;
+
+    return (
+      <User isLoading={isLoading} name={name} avatar={avatar} email={email} />
+    );
+  }
+
+  async componentDidMount() {
+    // Load async data.
+    // Update state with new data.
+    // Re-render our component.
+    let userData = await API.get('/', {
+      params: {
+        results: 1,
+        inc: 'name,email,picture'
+      }
+    });
+
+    userData = userData.data.results[0];
+
+    const name = `${userData.name.first} ${userData.name.last}`;
+    const avatar = userData.picture.large;
+    const email = userData.email;
+    // Update state with new data and re-render our component.
+
+    this.setState({
+      ...this.state, ...{
+        isLoading: false,
+        name,
+        avatar,
+        email
+      }
+    });
   }
 }
 
